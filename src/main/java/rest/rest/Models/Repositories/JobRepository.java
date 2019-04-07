e47c7208daba3f04cada788d51320dc715b6495b
ahumaidat <ahumaidat@atypon.com>
2019-04-07 13:37:33 +0300

    LIT-268457 ongoing: Build dependency API

--- src/main/java/rest/rest/Models/Repositories/JobRepository.java
+++ src/main/java/rest/rest/Models/Repositories/JobRepository.java
@@ -3,9 +3,6 @@ package rest.rest.Models.Repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import rest.rest.Models.Dependency.Jobs.Jobs;
-import rest.rest.Shared.dto.JobsMongoDbDTO;
-
-import java.util.List;
 
 @Repository
 public interface JobRepository extends MongoRepository<Jobs, String> {
